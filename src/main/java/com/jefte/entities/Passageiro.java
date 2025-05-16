176d84236f72fdccd8adace12b5f1b00e17edb9d
Dudu! <83459496+dudurtg2@users.noreply.github.com>
2025-05-16 00:33:30 -0300

    first commit

---
+++ src/main/java/com/jefte/entities/Passageiro.java
@@ -0,0 +1,54 @@
+package com.jefte.entities;
+
+
+public class Passageiro {
+    private int id;
+    private String nome;
+    private String cpf;
+    private int idade;
+
+
+    public Passageiro() {
+    }
+
+    public Passageiro(int id, String nome, String cpf, int idade) {
+        this.id = id;
+        this.nome = nome;
+        this.cpf = cpf;
+        this.idade = idade;
+    }
+
+    public int getId() {
+        return this.id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return this.nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return this.cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public int getIdade() {
+        return this.idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    
+}
---
+++ src/main/java/com/jefte/repositories/PassageiroRepository.java
@@ -0,0 +1,39 @@
+package com.jefte.repositories;
+
+import com.jefte.entities.Passageiro;
+
+import java.util.ArrayList;
+import java.util.List;
+public class PassageiroRepository {
+    private List<Passageiro> passageiros = new ArrayList<Passageiro>();
+
+    public List<Passageiro> getAll() {
+        return passageiros;
+    }
+
+    public Passageiro getById(int id) {
+        Passageiro passageiro = passageiros.stream().filter(p -> p.getId() == id).findFirst().get();
+        return passageiro;
+    }
+
+    public boolean exists(int id) {
+        return passageiros.stream().anyMatch(p -> p.getId() == id);
+    }
+
+    public void append(Passageiro passageiro) {
+        passageiros.add(passageiro);
+    }
+
+    public void remove(int id) {
+        passageiros.removeIf(passageiro -> passageiro.getId() == id);
+    }
+
+    public void update(int id, Passageiro passageiro) {
+        Passageiro PassageiroInDb = passageiros.stream().filter(p -> p.getId() == id).findFirst().get();
+
+        PassageiroInDb.setId(passageiro.getId());
+        PassageiroInDb.setNome(passageiro.getNome());
+        PassageiroInDb.setCpf(passageiro.getCpf());
+        PassageiroInDb.setIdade(passageiro.getIdade());
+    }
+}
